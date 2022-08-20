14d06e347de93dd63b5e71b910d5f849fe76fb73
emiliobresolin <emiliotdm@hotmail.com>
2022-08-20 14:29:06 -0300

    product controller implementation

---
+++ GeekShooping.ProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using GeekShooping.ProductAPI.Data.ValueObjects;
+using GeekShooping.ProductAPI.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShooping.ProductAPI.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private IProductRepository _repository;
+        public ProductController(IProductRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductVO>>> FindAll()
+        {
+            var products = await _repository.FindAll();
+            return Ok(products);
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductVO>> FindById(long id)
+        {
+            var product = await _repository.FindById(id);
+            if (product == null) return NotFound();
+            return Ok(product);
+        }
+    }
+}
