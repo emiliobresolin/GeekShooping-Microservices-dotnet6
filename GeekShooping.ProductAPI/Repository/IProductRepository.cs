f0b649bde714908a488f2094fc6acc3960850c7b
emiliobresolin <emiliotdm@hotmail.com>
2022-08-20 12:51:19 -0300

    IProductRepository definitions created

--- GeekShooping.ProductAPI/Repository/IProductRepository.cs
+++ GeekShooping.ProductAPI/Repository/IProductRepository.cs
@@ -1,7 +1,15 @@
-﻿namespace GeekShooping.ProductAPI.Repository
+﻿
+using GeekShooping.ProductAPI.Data.ValueObjects;
+
+namespace GeekShooping.ProductAPI.Repository
 {
     public interface IProductRepository
     {
+        Task<IEnumerable<ProductVO>> FindAll();
+        Task<ProductVO> FindById(long id);
+        Task<ProductVO> Create(ProductVO vo);
+        Task<ProductVO> Update(ProductVO vo);
+        Task<bool> Delete(long id);
 
     }
 }
