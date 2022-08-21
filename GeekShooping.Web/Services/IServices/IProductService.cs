a83319f2233c03d218e3c6f17138118fa207c483
emiliobresolin <emiliotdm@hotmail.com>
2022-08-21 11:47:16 -0300

    IProductService defined

---
+++ GeekShooping.Web/Services/IServices/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using GeekShooping.Web.Models;
+
+namespace GeekShooping.Web.Services.IServices
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductModel>> FindAllProducts();
+        Task<ProductModel> FindProductById(long id);
+        Task<ProductModel> CreateProduct(ProductModel model);
+        Task<ProductModel> UpdateProduct(ProductModel model);
+        Task<bool> DeleteProductById(long id);
+    }
+}
