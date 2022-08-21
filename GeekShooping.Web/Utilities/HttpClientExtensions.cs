8661babb5b732843dd0d28630883e08f05d30f95
emiliobresolin <emiliotdm@hotmail.com>
2022-08-21 12:01:45 -0300

    class httpclient implementation

---
+++ GeekShooping.Web/Utilities/HttpClientExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+
+namespace GeekShooping.Web.Utilities
+{
+    public static class HttpClientExtensions
+    {
+        public static async Task<T> ReadContentTAs<T>(this HttpResponseMessage response)
+        {
+            if (!response.IsSuccessStatusCode) 
+                throw new ApplicationException(
+                    $"something went wrong when calling the API: " + 
+                    $"{response.ReasonPhrase}");
+            //convertendo o JSON para objeto:
+            var dataAsString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            return JsonSerializer.Deserialize<T>(dataAsString, new JsonSerializerOptions 
+            {
+                PropertyNameCaseInsensitive = true
+            });
+        }
+    }
+}
