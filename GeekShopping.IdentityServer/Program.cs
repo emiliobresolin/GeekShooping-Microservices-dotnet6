c39dca7c5fd32ce887fb9d302f8867c20ba82846
emiliobresolin <emiliotdm@hotmail.com>
2022-09-21 20:37:54 -0300

    adding database and security configuration

--- GeekShopping.IdentityServer/Program.cs
+++ GeekShopping.IdentityServer/Program.cs
@@ -1,6 +1,33 @@
-var builder = WebApplication.CreateBuilder(args);
+using GeekShopping.IdentityServer.Configuration;
+using GeekShopping.IdentityServer.Model;
+using GeekShopping.IdentityServer.Model.Context;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
+var builder = WebApplication.CreateBuilder(args);
+var connection = builder.Configuration["MySqlConnection:MysqlConnectionString"];
 // Add services to the container.
+//aqui se adiciona os servicos, ao inves de service.AddDatabse... usamos builder.Service
+builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(connection,
+    new MySqlServerVersion(new Version(8, 0, 26))));
+// Add services to the container.
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<MySQLContext>()
+    .AddDefaultTokenProviders();
+builder.Services.AddIdentityServer(options =>
+{
+    options.Events.RaiseErrorEvents = true;
+    options.Events.RaiseInformationEvents = true;
+    options.Events.RaiseFailureEvents = true;
+    options.Events.RaiseSuccessEvents = true;
+    options.EmitStaticAudienceClaim = true;
+}).AddInMemoryIdentityResources(IdentityConfiguration.IdentityResources)
+.AddInMemoryClients(IdentityConfiguration.Clients)
+.AddAspNetIdentity<ApplicationUser>();
+// first try! lets see how it goes:
+builder.Services.AddIdentityServer().AddDeveloperSigningCredential();
+
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -13,7 +40,7 @@ if (!app.Environment.IsDevelopment())
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseIdentityServer();
 app.UseAuthorization();
 
 app.MapControllerRoute(
