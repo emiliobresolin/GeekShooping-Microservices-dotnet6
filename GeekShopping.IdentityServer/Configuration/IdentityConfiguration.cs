00e2657ba36c0f19383f0357c4c5cac0f73fee3d
emiliobresolin <emiliotdm@hotmail.com>
2022-09-21 19:35:24 -0300

    client configuration identityserver class

--- GeekShopping.IdentityServer/Configuration/IdentityConfiguration.cs
+++ GeekShopping.IdentityServer/Configuration/IdentityConfiguration.cs
@@ -1,4 +1,5 @@
-﻿using Duende.IdentityServer.Models;
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
 
 namespace GeekShopping.IdentityServer.Configuration
 {
@@ -27,6 +28,21 @@ namespace GeekShopping.IdentityServer.Configuration
                 ClientSecrets = { new Secret("my_super_secrete".Sha256())},
                 AllowedGrantTypes = GrantTypes.ClientCredentials,
                 AllowedScopes = {"read", "write", "profile"}
+            },
+            new Client
+            {
+                ClientId = "geek_shopping",
+                ClientSecrets = { new Secret("my_super_secrete".Sha256())},
+                AllowedGrantTypes = GrantTypes.Code,
+                RedirectUris = {"http://localhost:13484/signin-oidc"},
+                PostLogoutRedirectUris = {"http://localhost:13484/signout-callback-oidc"},
+                AllowedScopes = new List<string>
+                {
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile,
+                    IdentityServerConstants.StandardScopes.Email,
+                    "geek_shopping"
+                }
             }
         };
 
