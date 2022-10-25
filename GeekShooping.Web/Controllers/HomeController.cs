743c6dd26481281910d1081d10d4c8463073095c
emiliobresolin <emiliotdm@hotmail.com>
2022-10-25 14:13:47 -0300

    redirecting login view implementation

--- GeekShooping.Web/Controllers/HomeController.cs
+++ GeekShooping.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using GeekShooping.Web.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -27,6 +28,16 @@ namespace GeekShooping.Web.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        [Authorize]
+        public async Task<IActionResult> Login()
+        {
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Logout()
+        {
+            return SignOut("Cookies", "oidc");
         }
     }
 }
